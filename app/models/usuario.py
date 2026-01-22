60359ff35731115a0c65a4e0ae9320643fb8c801
Eneko Errazquin <ikcpt@plaiaundi.net>
2026-01-22 13:32:50 +0100

    auth_controller, form, usuario, login, registro

---
+++ app/models/usuario.py
@@ -0,0 +1,19 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
+
+class Usuario(UserMixin, db.Model):
+    __tablename__='usuarios'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<Usuario {self.username}'
